8d41a7186aaafe7afe399e78ff4154b692518913
Mehdi Sadighian <mehdi.sadighian@hotmail.com>
2024-02-11 17:31:25 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <iostream>
+#include <unistd.h>
+
+#include "KavehNegar_Example.h"
+
+using namespace std;
+
+int main()
+{
+    KavehNegarExample();
+
+    sleep(100);
+}
